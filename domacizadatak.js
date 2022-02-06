8df487fdda121cd47aea9fc61bde2b751762cc82
FedorPop <fedor.popovic30@gmail.com>
2022-02-06 19:14:18 +0100

    domacizadatak

---
+++ domacizadatak.js
@@ -0,0 +1,13 @@
+var a
+var b
+var c
+function domacizadatak(a, b, c) {
+    if (a > 0) {
+      return b+c;
+    } else {
+      return b-c;
+    }
+    
+  }
+  
+  console.log(domacizadatak(1, 6, 9));
